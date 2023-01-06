9a62fb2387060e5dfc3eaf1dac0c1f5a7e6d7378
K4Lok <samkalok107@gmail.com>
2023-01-07 07:08:55 +0800

    feature integrate PWA

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -15,4 +22,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/pwa/icon-192x192.png" />
+        <meta name='theme-color' content="#FFFFFF"></meta>
+      </Head>
       <body className='overflow-x-hidden'>
         <Main />
         <NextScript />
