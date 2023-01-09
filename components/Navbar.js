0aa88f130187065f03a05f74b54371fa23c30de0
K4Lok <samkalok107@gmail.com>
2023-01-09 12:31:55 +0800

    add github icon on the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -28,8 +28,13 @@ export default function Navbar() {
 	return (
         <header>
 			<div ref={navRef} className={`fixed top-0 w-full py-wrapper bg-white/90 z-50 transition duration-500 ${showNav ? 'translate-y-0' : '-translate-y-full'}`}>
-				<div className="max-w-container mx-auto px-wrapper xl:px-0">
-					<Link href={'/'}><Image priority className='active:scale-[101%]' src={'/icon.svg'} alt={'icon'} width={200} height={50} /></Link>
+				<div className="max-w-container mx-auto px-wrapper xl:px-0 flex justify-between items-baseline">
+					<Link href={'/'}>
+						<Image priority className='active:scale-[101%]' src={'/icon.svg'} alt={'icon'} width={200} height={50} />
+						</Link>
+					<Link href={'https://github.com/k4lok'} target={'_blank'}>
+						<Image priority className='active:scale-[101%] hover:brightness-110' src={'/github.svg'} alt={'github icon'} width={40} height={40} />
+					</Link>
 				</div>
 			</div>
 		</header>
