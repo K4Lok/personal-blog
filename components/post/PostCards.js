bc17358775f5d59ef431abab4394fb7d41a4cf52
K4Lok <samkalok107@gmail.com>
2023-01-19 12:29:19 +0800

    update english date format on PostCard

--- components/post/PostCards.js
+++ components/post/PostCards.js
@@ -1,19 +1,23 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 import PostCard from "./PostCard"
 
 export default function PostCards({ posts, locale }) {
-  return (
-    <section id="posts">
-        <div className="w-full">
-            <ul className="flex flex-col space-y-3">
-                {posts.map((post) => { 
-                    const date = new Intl.DateTimeFormat('zh-MO', { dateStyle: 'long', timeZone: 'Asia/Macau' }).format(new Date(post.createdAt))
-                    return (
-                        <PostCard key={post.slug} ard title={post.title} slug={post.slug} categories={post.categories} date={date}/>
-                    )
-                })}
-            </ul>
-        </div>
-    </section>
-  )
+	const router = useRouter()
+
+	return (
+		<section id="posts">
+			<div className="w-full">
+				<ul className="flex flex-col space-y-3">
+					{posts.map((post) => {
+						const { locale } = router
+						const date = new Intl.DateTimeFormat(locale == 'zh' ? 'zh-MO' : locale, { dateStyle: 'long', timeZone: 'Asia/Macau' }).format(new Date(post.createdAt))
+						return (
+							<PostCard key={post.slug} ard title={post.title} slug={post.slug} categories={post.categories} date={date} />
+						)
+					})}
+				</ul>
+			</div>
+		</section>
+	)
 }
