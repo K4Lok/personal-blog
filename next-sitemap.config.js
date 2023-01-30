ead444bbe231d9a1b942aa42e89a87fbb8bd42b8
K4Lok <samkalok107@gmail.com>
2023-01-30 23:42:18 +0800

    init sitemap config with excluding /en path

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://blog.samkalok.com',
+    generateRobotsTxt: true,
+    exclude: [
+        "*/en/*"
+    ],
+};
