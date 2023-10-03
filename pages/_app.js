103bac4480c7b7f2e76364f3d65d35e34b6b1fd7
ImK4Lok <samkalok107@gmail.com>
2023-10-03 15:11:28 +0800

    add: vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import { NextIntlProvider } from 'next-intl'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <NextIntlProvider messages={pageProps.messages}>
       <Component {...pageProps} />
+      <Analytics />
     </NextIntlProvider>
   )
 }
